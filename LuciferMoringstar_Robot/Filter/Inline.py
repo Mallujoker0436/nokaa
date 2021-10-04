5005040bde74c8036c02c122705f31b37a4299db
Mallujoker0436 <89502833+Mallujoker0436@users.noreply.github.com>
2021-10-04 12:51:21 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,7 +89,7 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Working for', url="https://t.me/matahdmovie123"
+        InlineKeyboardButton('working for', url=f'https://t.me/matahdmovie123')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
